38deed5adbe6f0de2ab637eb2f0cfbbc34964205
Zac <TXTCLASS@users.noreply.github.com>
2017-10-12 13:26:59 -0400

    Update getTx.js

--- scripts/getTx.js
+++ scripts/getTx.js
@@ -1,7 +1,7 @@
 /*
 A script that gets the transaction info from a transaction hash/ID, then shows the result in the console.
 
-For an explanation of this code, navigate to this url https://github.com/TXTCLASS/Web3byExample/blob/master/scripts/getTx.md
+For an explanation of this code, navigate to the wiki https://github.com/TXTCLASS/Web3byExample/wiki/Get-Transaction
 */
 
 // Get the web3 Module
